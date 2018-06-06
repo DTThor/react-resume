ac2ae3440a5b840fd46309900b51221d413c5467
Dylan Thorwaldson <dtthor@Dylanns-MacBook-Pro.local>
2018-06-06 14:44:08 -0500

    Add all html and styles from old resume project into App.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         use: 'babel-loader',
         exclude: /node_modules/
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ]
   },
   plugins: [
